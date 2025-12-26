cc4b4cf76a2617ff3a68ddedee3a45d5e9fa0da2
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-12-26 11:47:33 -0800

    Implement basic TCP server

--- common/foo.go
+++ common/foo.go
@@ -1,3 +1,9 @@
 package common
 
 var Foo = 5
+
+func Must(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 
 	"github.com/lukejianu/gogame/common"
 )
 
+var port = 8080
+var address = fmt.Sprintf(":%d", port)
+
 func main() {
-	fmt.Printf("Hello, world %d!\n", common.Foo)
+	l, err := net.Listen("tcp", address)
+	common.Must(err)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		common.Must(err)
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scan := bufio.NewScanner(conn)
+	for scan.Scan() {
+		fmt.Println(scan.Text())
+	}
 }
 
