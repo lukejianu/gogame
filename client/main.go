571561eea06fcc1cd2360ff5fdbf5b36c81a9610
lukejianu <71729102+lukejianu@users.noreply.github.com>
2025-12-28 11:29:47 -0800

    Refactor client

--- client/main.go
+++ client/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 
 	"github.com/lukejianu/gogame/common"
 
@@ -18,33 +19,66 @@ var port = 8080
 var address = fmt.Sprintf(":%d", port)
 
 var red = color.NRGBA{255, 0, 0, 255}
+var blue = color.NRGBA{0, 0, 255, 255}
 
 type Game struct {
-	positions []common.Position
-	writer    io.Writer
+	state common.ClientGameState
+
+	updates []common.ClientGameState
+	mu      sync.Mutex
+
+	writer io.Writer
 }
 
 func (g *Game) Update() error {
-	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		msg := common.SerializeMoveInput(common.MoveLeftInput)
-		_, err := g.writer.Write(msg)
-		common.Must(err)
-	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
-		msg := common.SerializeMoveInput(common.MoveRightInput)
-		_, err := g.writer.Write(msg)
-		common.Must(err)
+	keyInputs := ebiten.InputChars()
+	for _, key := range keyInputs {
+		g.handleKeyInput(key)
 	}
+	updateState(g)
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	for _, p := range g.positions {
-		DrawCircle(screen, p)
+func (g *Game) handleKeyInput(key rune) {
+	mi, badKey := keyToMoveInput(key)
+	if badKey {
+		return
+	}
+	msg := common.SerializeMoveInput(mi)
+	_, err := g.writer.Write(msg)
+	common.Must(err)
+	// TODO: Predict.
+}
+
+func keyToMoveInput(key rune) (common.MoveInput, bool) {
+	switch key {
+	case 'a':
+		return common.MoveLeftInput, false
+	case 'd':
+		return common.MoveRightInput, false
+	default:
+		return common.MoveInput(0), true
 	}
 }
 
-func DrawCircle(screen *ebiten.Image, x int) {
-	vector.FillCircle(screen, float32(x), 250, 25, red, false)
+func updateState(g *Game) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	if len(g.updates) > 0 {
+		g.state = g.updates[0]
+		g.updates = g.updates[1:]
+	}
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	DrawCircle(screen, g.state.You, blue)
+	for _, p := range g.state.Others {
+		DrawCircle(screen, p, red)
+	}
+}
+
+func DrawCircle(screen *ebiten.Image, x int, c color.Color) {
+	vector.FillCircle(screen, float32(x), 250, 25, c, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -55,8 +89,7 @@ func main() {
 	conn := connect()
 	defer conn.Close()
 	g := &Game{
-		positions: []common.Position{},
-		writer:    common.NewLineWriter(conn),
+		writer: common.NewLineWriter(conn),
 	}
 	go listenForUpdates(g, conn)
 	if err := runGame(g, conn); err != nil {
@@ -75,13 +108,14 @@ func listenForUpdates(g *Game, conn net.Conn) {
 	for scan.Scan() {
 		line := scan.Bytes()
 		cgs := common.DeserializeClientGameState(line)
-		positions := []common.Position{}
-		positions = append(positions, cgs.You)
-		for _, p := range cgs.Others {
-			positions = append(positions, p)
-		}
-		g.positions = positions // TODO: Lock.
+		appendUpdate(g, cgs)
 	}
+}
+
+func appendUpdate(g *Game, cgs common.ClientGameState) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	g.updates = append(g.updates, cgs)
 }
 
 func connect() net.Conn {
